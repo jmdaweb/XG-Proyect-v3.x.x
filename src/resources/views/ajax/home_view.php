513f6dd0e941d4f266ce212c1bcab1cabb9769b8
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2022-08-18 13:28:07 +0200

    Ajax home view: removed overlay class on rules button

--- src/resources/views/ajax/home_view.php
+++ src/resources/views/ajax/home_view.php
@@ -2,10 +2,10 @@
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><style type="text/css"></style></head><body><div class="inner-box clearfix">
             <h2>{aj_home_conquer_universe}</h2>
             <p>{aj_home_description}</p>
-            <a class="overlay button" href="index.php?page=rules" title="{aj_home_rules}">{aj_home_rules}</a>
+            <a target="_blank" class="button" href="index.php?page=rules" title="{aj_home_rules}">{aj_home_rules}</a>
         </div>
 
-        <div id="trailer" class="inner-box last clearfix">
+        <!--<div id="trailer" class="inner-box last clearfix">
             <h2 id="trailer">{aj_home_trailer}</h2>
             <div id="flashTrailer">
                 <object width="425" height="270">
@@ -15,7 +15,7 @@
 
                 </object>
             </div>
-        </div>
+        </div>-->
 
         <script type="text/javascript">
             // Trailer-Klick-Error-Ausblendung.
